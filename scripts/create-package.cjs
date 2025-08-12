45696bb9fc8f7444c11a7643504b8545459baf4f
김성득 <sdkim@ibleaders.co.kr>
2025-08-12 12:38:13 +0900

    package.json 정보 수정

--- scripts/create-package.cjs
+++ scripts/create-package.cjs
@@ -5,13 +5,19 @@ const outDir = path.resolve(__dirname, '../dist/ibsheet-interface');
 const packageJsonPath = path.join(outDir, 'package.json');
 
 const packageJsonContent = {
-  name: "ibsheet-interface",
+  name: "@ibsheet/interface",
   version: "1.0.0",
   main: "./index.js",
   module: "./index.js",
   types: "./index.d.ts",
   license: "MIT",
-  private: false
+  repository: {
+    type: 'git',
+    url: 'git+https://github.com/ibsheet/ibsheet-interface.git',
+  },
+  keywords: ['IBSheet', 'ibsheet', 'typescript', 'interface'],
+  author: 'IB Leaders <support@ibleaders.co.kr>',
+  description: 'TypeScript interface for IBSheet',
 };
 
 fs.mkdirSync(outDir, { recursive: true });
